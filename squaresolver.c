54e9cbe50e1607fde0890694721368af641eff32
evanvd <ivanvadeev7347@gmail.com>
2025-08-22 15:27:54 +0300

    with_magic_numbers

--- squaresolver.c
+++ squaresolver.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <cmath>
+
+const double EPS = 1e-6;
+
+// TODO solveLinear
 int line(double b, double c, double* x1)
 {
-    if (fabs(b-0) < 0.0001)
+    // TODO use abs()
+    if (abs(b-0) < 0.0001) // TODO use EPS
     {
-        if (fabs(b-0) < 0.0001)
+        if (abs(c-0) < 0.0001)
         {
             return -1;
         }
-        if (fabs(c-0) > 0.0001)
+
+        if (abs(c-0) > 0.0001)
         {
             return 0;
         }
+
         return -2;
     }
     else
@@ -19,11 +26,13 @@ int line(double b, double c, double* x1)
         *x1 = -c/b;
         return 1;
     }
+    return -2;
 }
 
-int square (double a, double b, double c, double* x1, double* x2)
+// TODO solveQuadratic
+int square (double a, double b, double c, double* x1, double* x2) // TODO redundant whitespace
 {
-    return -2;
+    //return -2;
     double d = b*b-4*a*c;
     if(d < 0)
     {
@@ -40,6 +49,7 @@ int square (double a, double b, double c, double* x1, double* x2)
         *x2 = (-b - sqrt(d))/(2*a);
         return 2;
     }
+    return -2;
 }
 
 // TODO blank line
@@ -63,8 +73,10 @@ int num_root(double a, double b, double c, double*x1,double*x2)
 int main(void) // TODO void is not necessary
 {
     printf("введите кенты квадратного уравнения вида ax^2+bx+c\n ");
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0; // TODO whitespaces
+
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     scanf("%lg%lg%lg", &a,&b,&c);
+
     int nroot = num_root(a,b,c,&x1,&x2);
     // TODO blank line
     if (nroot == 0)
@@ -73,11 +85,16 @@ int main(void) // TODO void is not necessary
     }
     if (nroot == 1)
     {
-        printf("уравнение имеет единственное решение %f ", x1);
+        printf("уравнение имеет единственное решение %f ", x1); // TODO format
     }
     if (nroot == 2 ) // TODO inconsistent codestyle
     {
-     printf("уравнение имеет 2 решения %f %f", x1, x2);
+     printf("уравнение имеет 2 решения %f %f", x1, x2); // TODO wrong format specifiers
     }
+    if (nroot == -1 )
+    {
+        printf("бесконечное количество решений");
+    }
+
     return 0;
 }
