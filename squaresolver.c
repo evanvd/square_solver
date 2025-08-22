2016c1f6fd45069814d0e438dcd4afd29e2cdad0
evanvd <ivanvadeev7347@gmail.com>
2025-08-22 13:34:50 +0300

    second

--- squaresolver.c
+++ squaresolver.c
@@ -1,35 +1,52 @@
 #include <stdio.h>
 #include <cmath>
-/*int square_solver(double a, double b, double c, double*x1,double*x2)
+int num_root(double a, double b, double c, double*x1,double*x2)
 {
 
-}*/
+    if (a==0)
+    {
+        *x1=-c/b;
+        return 1;
+    }
+    else
+    {
+        double d = b*b-4*a*c;
+        if(d < 0)
+        {
+            return 0;
+        }
+        if (d==0)
+        {
+            *x1= -b/(2*a);
+            return 1;
+        }
+        if (d > 0)
+        {
+            *x1 = (-b + sqrt(d))/(2*a);
+            *x2 = (-b - sqrt(d))/(2*a);
+            return 2;
+        }
+    }
+}
 
 
 int main(void)
 {
-    printf("введите кенты квадратного уравнения вида ax^2+bx+c\n a:");
-    double a=0, b=0,c=0,x1,x2;
-    scanf("%lg",&a);
-    printf("b: ");
-    scanf("%lg",&b);
-    printf("c: ");
-    scanf("%lg",&c);
-    double d = b*b-4*a*c;
-    if(d<0)
+    printf("введите кенты квадратного уравнения вида ax^2+bx+c\n ");
+    double a=0, b=0,c=0,x1=0,x2=0;
+    scanf("%lg%lg%lg", &a,&b,&c);
+    int nroot = num_root(a,b,c,&x1,&x2);
+    if (nroot == 0)
     {
-        printf("нет действительных корней");
+        printf("уравнение не имеет решений");
     }
-    if (d==0)
+    if (nroot == 1)
     {
-        x1=-b/(2*a);
-        printf("единственный корень %.2f\n",x1);
+        printf("уравнение имеет единственное решение %f ", x1);
     }
-    if (d>0)
+    if (nroot == 2 )
     {
-        x1= (-b + sqrt(d))/(2*a);
-        x2= (-b - sqrt(d))/(2*a);
-        printf("первый корень %.2f\nвторой корень %.2f\n",x1,x2);
+     printf("уравнение имеет 2 решения %f %f", x1, x2);
     }
     return 0;
 }
