486c14cc0b6077c170938a166365b6e0d200b73c
evanvd <ivanvadeev7347@gmail.com>
2025-08-22 17:33:03 +0300

    with_enum

--- squaresolver.c
+++ squaresolver.c
@@ -1,97 +1,102 @@
 #include <stdio.h>
 #include <cmath>
 
+
 const double EPS = 1e-6;
+enum RootsCount {
+Error = -2,
+InfiniteRoots = -1,
+ZeroRoots = 0,
+OneRoot = 1,
+TwoRoots = 2
+};
+enum RootsCount solve_linear(double b, double c, double* x1);
+enum RootsCount solveQuadratic (double a, double b, double c, double* x1, double* x2);
+enum RootsCount num_root(double a, double b, double c, double*x1,double*x2);
 
-// TODO solveLinear
-int line(double b, double c, double* x1)
+
+enum RootsCount solve_linear(double b, double c, double* x1)
 {
-    // TODO use abs()
-    if (abs(b-0) < 0.0001) // TODO use EPS
+    if (fabs(b) < EPS)
     {
-        if (abs(c-0) < 0.0001)
+        if (fabs(c) < EPS)
         {
-            return -1;
+            return InfiniteRoots;
         }
 
-        if (abs(c-0) > 0.0001)
+        if (fabs(c) > EPS)
         {
-            return 0;
+            return ZeroRoots;
         }
 
-        return -2;
+        return Error;
     }
     else
     {
         *x1 = -c/b;
-        return 1;
+        return OneRoot;
     }
-    return -2;
+    return Error;
 }
 
-// TODO solveQuadratic
-int square (double a, double b, double c, double* x1, double* x2) // TODO redundant whitespace
+enum RootsCount solveQuadratic (double a, double b, double c, double* x1, double* x2)
 {
-    //return -2;
+
     double d = b*b-4*a*c;
     if(d < 0)
     {
-        return 0;
+        return ZeroRoots;
     }
-    if (fabs(d-0) < 0.0001)
+    if (fabs(d) < EPS)
     {
         *x1= -b/(2*a);
-        return 1;
+        return OneRoot;
     }
     if (d > 0)
     {
         *x1 = (-b + sqrt(d))/(2*a);
         *x2 = (-b - sqrt(d))/(2*a);
-        return 2;
+        return TwoRoots;
     }
-    return -2;
+    return Error;
 }
 
 // TODO blank line
-int num_root(double a, double b, double c, double*x1,double*x2)
+enum RootsCount num_root(double a, double b, double c, double*x1,double*x2)
 {
-    // TODO split into two functions
-    int nroot = 0;
-    if (fabs(a-0) < 0.0001) // TODO read about double comparisons
+    if (fabs(a) < EPS)
     {
-        nroot = line(b,c,x1);
-        return nroot;
+        return solve_linear(b,c,x1);
     }
     else
     {
-        nroot = square(a,b,c,x1,x2);
-        return nroot;
+        return solveQuadratic(a,b,c,x1,x2);
     }
 }
 
 
-int main(void) // TODO void is not necessary
+int main()
 {
     printf("введите кенты квадратного уравнения вида ax^2+bx+c\n ");
 
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     scanf("%lg%lg%lg", &a,&b,&c);
 
-    int nroot = num_root(a,b,c,&x1,&x2);
-    // TODO blank line
-    if (nroot == 0)
+    enum RootsCount nroot = num_root(a,b,c,&x1,&x2);
+
+    if (nroot == ZeroRoots)
     {
         printf("уравнение не имеет решений");
     }
-    if (nroot == 1)
+    if (nroot == OneRoot)
     {
-        printf("уравнение имеет единственное решение %f ", x1); // TODO format
+        printf("уравнение имеет единственное решение %lf ", x1);
     }
-    if (nroot == 2 ) // TODO inconsistent codestyle
+    if (nroot == TwoRoots)
     {
-     printf("уравнение имеет 2 решения %f %f", x1, x2); // TODO wrong format specifiers
+     printf("уравнение имеет 2 решения %lf %lf", x1, x2);
     }
-    if (nroot == -1 )
+    if (nroot == InfiniteRoots)
     {
         printf("бесконечное количество решений");
     }
