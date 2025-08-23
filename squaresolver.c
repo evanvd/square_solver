9b644172491c6a0bf8fe1935d593e04f45195edf
evanvd <ivanvadeev7347@gmail.com>
2025-08-23 10:14:51 +0300

    no_assert

--- squaresolver.c
+++ squaresolver.c
@@ -2,20 +2,45 @@
 #include <cmath>
 
 
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠛⢉⢉⠉⠉⠻⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⠟⠠⡰⣕⣗⣷⣧⣀⣅⠘⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⠃⣠⣳⣟⣿⣿⣷⣿⡿⣜⠄⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⡿⠁⠄⣳⢷⣿⣿⣿⣿⡿⣝⠖⠄⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⠃⠄⢢⡹⣿⢷⣯⢿⢷⡫⣗⠍⢰⣿⣿⣿⣿⣿
+// ⣿⣿⣿⡏⢀⢄⠤⣁⠋⠿⣗⣟⡯⡏⢎⠁⢸⣿⣿⣿⣿⣿
+// ⣿⣿⣿⠄⢔⢕⣯⣿⣿⡲⡤⡄⡤⠄⡀⢠⣿⣿⣿⣿⣿⣿
+// ⣿⣿⠇⠠⡳⣯⣿⣿⣾⢵⣫⢎⢎⠆⢀⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⠄⢨⣫⣿⣿⡿⣿⣻⢎⡗⡕⡅⢸⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⠄⢜⢾⣾⣿⣿⣟⣗⢯⡪⡳⡀⢸⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⠄⢸⢽⣿⣷⣿⣻⡮⡧⡳⡱⡁⢸⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⡄⢨⣻⣽⣿⣟⣿⣞⣗⡽⡸⡐⢸⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⡇⢀⢗⣿⣿⣿⣿⡿⣞⡵⡣⣊⢸⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⡀⡣⣗⣿⣿⣿⣿⣯⡯⡺⣼⠎⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣧⠐⡵⣻⣟⣯⣿⣷⣟⣝⢞⡿⢹⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⡆⢘⡺⣽⢿⣻⣿⣗⡷⣹⢩⢃⢿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣷⠄⠪⣯⣟⣿⢯⣿⣻⣜⢎⢆⠜⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⡆⠄⢣⣻⣽⣿⣿⣟⣾⡮⡺⡸⠸⣿⣿⣿⣿
+// ⣿⣿⡿⠛⠉⠁⠄⢕⡳⣽⡾⣿⢽⣯⡿⣮⢚⣅⠹⣿⣿⣿
+// ⡿⠋⠄⠄⠄⠄⢀⠒⠝⣞⢿⡿⣿⣽⢿⡽⣧⣳⡅⠌⠻⣿
+// ⠁⠄⠄⠄⠄⠄⠐⡐⠱⡱⣻⡻⣝⣮⣟⣿⣻⣟⣻⡺⣊
+
 const double EPS = 1e-6;
 enum RootsCount {
-Error = -2,
-InfiniteRoots = -1,
-ZeroRoots = 0,
-OneRoot = 1,
-TwoRoots = 2
+    Error = -2,
+    InfiniteRoots = -1,
+    ZeroRoots = 0,
+    OneRoot = 1,
+    TwoRoots = 2
 };
+
 enum RootsCount solve_linear(double b, double c, double* x1);
-enum RootsCount solveQuadratic (double a, double b, double c, double* x1, double* x2);
+enum RootsCount solveQuadratic(double a, double b, double c, double* x1, double* x2);
 enum RootsCount num_root(double a, double b, double c, double*x1,double*x2);
+void print_roots(enum RootsCount nroot, double x1, double x2);
 
+// TODO asserts
 
-enum RootsCount solve_linear(double b, double c, double* x1)
+RootsCount solve_linear(double b, double c, double* x1)
 {
     if (fabs(b) < EPS)
     {
@@ -36,54 +61,47 @@ enum RootsCount solve_linear(double b, double c, double* x1)
         *x1 = -c/b;
         return OneRoot;
     }
+
     return Error;
 }
 
-enum RootsCount solveQuadratic (double a, double b, double c, double* x1, double* x2)
+RootsCount solveQuadratic(double a, double b, double c, double* x1, double* x2)
 {
+    double d = b*b - 4*a*c;
 
-    double d = b*b-4*a*c;
-    if(d < 0)
+    if(d < EPS)
     {
         return ZeroRoots;
     }
+
     if (fabs(d) < EPS)
     {
         *x1= -b/(2*a);
         return OneRoot;
     }
-    if (d > 0)
+
+    if (d > EPS)
     {
+
         *x1 = (-b + sqrt(d))/(2*a);
         *x2 = (-b - sqrt(d))/(2*a);
         return TwoRoots;
     }
+
     return Error;
 }
 
-// TODO blank line
-enum RootsCount num_root(double a, double b, double c, double*x1,double*x2)
+RootsCount num_root(double a, double b, double c, double*x1,double*x2)
 {
     if (fabs(a) < EPS)
     {
         return solve_linear(b,c,x1);
     }
-    else
-    {
-        return solveQuadratic(a,b,c,x1,x2);
-    }
+    return solveQuadratic(a,b,c,x1,x2);
 }
 
-
-int main()
+void print_roots(enum RootsCount nroot, double x1, double x2)
 {
-    printf("введите кенты квадратного уравнения вида ax^2+bx+c\n ");
-
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    scanf("%lg%lg%lg", &a,&b,&c);
-
-    enum RootsCount nroot = num_root(a,b,c,&x1,&x2);
-
     if (nroot == ZeroRoots)
     {
         printf("уравнение не имеет решений");
@@ -100,6 +118,19 @@ int main()
     {
         printf("бесконечное количество решений");
     }
+}
 
+int main()
+{
+    printf("введите кенты квадратного уравнения вида ax^2+bx+c\n ");
+
+    // TODO init with nan
+    double a = NAN, b = NAN, c = NAN;
+    scanf("%lg%lg%lg", &a, &b, &c);
+
+    double x1 = NAN, x2 = NAN;
+    enum RootsCount nroot = num_root(a, b, c, &x1, &x2);
+
+    print_roots(nroot, x1, x2);
     return 0;
 }
