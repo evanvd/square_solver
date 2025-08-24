23155d6114f409cfc5fda77e9a0d50022e6da4e8
evanvd <ivanvadeev7347@gmail.com>
2025-08-24 13:46:22 +0300

    tests

--- squaresolver.cpp
+++ squaresolver.cpp
@@ -2,10 +2,10 @@
 #include <math.h>
 #include <assert.h>
 
+
 const double EPS = 1e-6;
 
-// TODO naming style
-enum RootsCount
+enum rootsCount
 {
     Error = -2,
     InfiniteRoots = -1,
@@ -14,31 +14,45 @@ enum RootsCount
     TwoRoots = 2
 };
 
-struct coeff { // TODO move { to next line
+struct coeff
+{
     double a;
     double b;
     double c;
 };
 
-struct roots { // TODO move { to next line
+struct roots
+{
     double x1;
     double x2;
-    enum RootsCount nroots;
+    enum rootsCount nroots;
 };
 
-RootsCount solve_linear(struct coeff coeff, struct roots* roots);
-RootsCount solve_quadratic(struct coeff coeff, struct roots* roots);
-RootsCount num_root(struct coeff coeff, struct roots* roots);
-void print_roots(struct roots roots);
-void TestSolve_quadratic();
-void TestSolve_linear();
-bool checkcase (struct coeff CheckCoeff, struct roots RightAnswer, struct roots* ActualAnswer);
-
-int main(int argc, char **argv)
+struct checkEquation
 {
-    //TestSolve_linear();
-    //TestSolve_quadratic();
+    struct coeff CheckCoeff;
+    struct roots RightAnswer;
+    struct roots ActualAnswer;
+};
 
+
+rootsCount solve_linear(struct coeff coeff, struct roots* roots);
+rootsCount solve_quadratic(struct coeff coeff, struct roots* roots);
+rootsCount num_root(struct coeff coeff, struct roots* roots);
+void print_roots(struct roots roots);
+
+void run_test();
+void print_test(checkEquation equation);
+bool check_case (struct coeff CheckCoeff, struct roots RightAnswer, struct roots* ActualAnswer);
+
+
+int main(char **argv)
+{
+    //if (argv[1] == "test")
+    {
+        run_test();
+    }
+    //printf(argv[1],"\n");
     printf("Введите коэффициенты квадратного уравнения вида ax^2+bx+c:\n ");
 
     struct coeff coeff = {NAN, NAN, NAN};
@@ -51,7 +65,8 @@ int main(int argc, char **argv)
     return 0;
 }
 
-RootsCount num_root(struct coeff coeff, struct roots* roots)
+
+rootsCount num_root(struct coeff coeff, struct roots* roots)
 {
     if (fabs(coeff.a) < EPS)
     {
@@ -60,7 +75,7 @@ RootsCount num_root(struct coeff coeff, struct roots* roots)
     return solve_quadratic(coeff, roots);
 }
 
-RootsCount solve_linear(struct coeff coeff, struct roots* roots)
+rootsCount solve_linear(struct coeff coeff, struct roots* roots)
 {
     if (fabs(coeff.b) < EPS)
     {
@@ -77,9 +92,9 @@ RootsCount solve_linear(struct coeff coeff, struct roots* roots)
     }
 }
 
-RootsCount solve_quadratic(struct coeff coeff, struct roots* roots)
+rootsCount solve_quadratic(struct coeff coeff, struct roots* roots)
 {
-    // assert on roots != NULL
+    assert(roots != NULL);
     assert(isfinite(coeff.a));
     assert(isfinite(coeff.b));
     assert(isfinite(coeff.c));
@@ -124,61 +139,69 @@ void print_roots(struct roots roots) // struct keyword not needed
             printf("Уравнение имеет бесконечно много решений\n");
             break;
         case Error:
-            printf("Ошибка в вычислениях\n");
+            printf("ERROR\n");
             break;
         default:
-            printf("Неизвестный тип решения\n");
             break;
     }
 }
 // TODO blank line
 
-// TODO split words in name
-bool checkcase (struct coeff CheckCoeff, struct roots RightAnswer, struct roots* ActualAnswer)
+bool check_case (struct coeff CheckCoeff, struct roots RightAnswer, struct roots* ActualAnswer)
 {
     *ActualAnswer = {NAN, NAN, Error};
     ActualAnswer->nroots = num_root(CheckCoeff, ActualAnswer);
     // Split line (too long)
-    if(fabs(ActualAnswer->nroots - RightAnswer.nroots) < EPS && (ActualAnswer->x1 - RightAnswer.x1) < EPS && (ActualAnswer->x2 - RightAnswer.x2) < EPS)
+    if(fabs(ActualAnswer->nroots - RightAnswer.nroots) < EPS )
     {
-        return 1; // true
+        if(ActualAnswer->nroots < OneRoot)
+        {
+            return true;
+        }
+        if(ActualAnswer->nroots == OneRoot && fabs(ActualAnswer->x1 - RightAnswer.x1) < EPS)
+        {
+            return true;
+        }
+        if(ActualAnswer->nroots == 2 && fabs(ActualAnswer->x1 - RightAnswer.x1) < EPS && fabs(ActualAnswer->x2 - RightAnswer.x2) < EPS)
+        {
+            return true;
+        }
     }
-    return 0; // false
+    return false;
 }
 // TODO remove lines
-//
-
 // NAMING CASES:
 // camelCase
 // PascalCase
 // snake_case
 // kebab-case -- not available in C
-
-// TODO test_solve_quadratic
-void TestSolve_quadratic()
+void print_test(checkEquation equation)
 {
-    // Тест 1: x^2 - 5x + 6 = 0 (корни: 2 и 3)
-    struct coeff TestCoeff = {1, -5, 6};
-    struct roots TestRoots = {2, 3, TwoRoots};
-    struct roots Answer = {NAN, NAN, Error};
-    if (!(checkcase(TestCoeff,TestRoots,&Answer)))
+    if(check_case(equation.CheckCoeff, equation.RightAnswer, &equation.ActualAnswer))
     {
-        // TODO separate function
-        printf("FAILED: solve_quadratic(1 -5 6) -> x1 = %lf, x2 = %lf (should be x1 = 3, x2 = 2)\n",Answer.x1, Answer.x2);
+        printf("PASSED (%lg %lg %lg)\n", equation.CheckCoeff.a, equation.CheckCoeff.b, equation.CheckCoeff.c);
     }
-    // Тест 2: 2x^2 - 4x + 2 = 0 (корень: 1)
-    TestCoeff = {2, -4, 2};
-    TestRoots = {2, 3, OneRoot};
-    if (!(checkcase(TestCoeff,TestRoots,&Answer)))
+    else
     {
-        printf("FAILED: solve_quadratic(2 -4 2) -> x1 = %lf, x2 =%lf (should be x = 1)\n",Answer.x1, Answer.x2);
+        printf("FAILED (%lg %lg %lg): ", equation.CheckCoeff.a, equation.CheckCoeff.b, equation.CheckCoeff.c );
+        printf("x1 = %lg x2 = %lg", equation.ActualAnswer.x1, equation.ActualAnswer.x2 );
+        printf(" (should be x1 = %lg x2= %lg)\n", equation.RightAnswer.x1, equation.RightAnswer.x2);
     }
-    TestCoeff = {6, 1, 2};
-    TestRoots = {2, 3, ZeroRoots};
-    if (!(checkcase(TestCoeff,TestRoots,&Answer)))
-    {
-        printf("FAILED: solve_quadratic(6 1 2) -> x1 = %lf, x2 = %lf (should be No Roots)\n",Answer.x1, Answer.x2);
+}
 
+void run_test()
+{
+    struct checkEquation TestEquation[] =
+    {
+        {.CheckCoeff = {1, -5, 6}, .RightAnswer = {3, 2, TwoRoots}},
+        {.CheckCoeff = {2, -4, 2},.RightAnswer = {1, NAN, OneRoot}},
+        {.CheckCoeff = {6, 2, 1}, .RightAnswer = {NAN, NAN, ZeroRoots}},
+        {.CheckCoeff = {0, 0, 0}, .RightAnswer = {NAN, NAN, InfiniteRoots}},
+        {.CheckCoeff = {0, 0, 1}, .RightAnswer = {NAN, NAN, ZeroRoots}},
+        {.CheckCoeff ={0, 1, -1}, .RightAnswer = {1, NAN, OneRoot}}
+    };
+    for (int i = 0; i < 6; i++)
+    {
+        print_test(TestEquation[i]);
     }
-
 }
